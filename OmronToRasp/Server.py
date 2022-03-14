9f1063d47a08c65c0413729f1b54a5a52f28741e
J-Dorey <101527759+J-Dorey@users.noreply.github.com>
2022-03-15 10:45:31 +1100

    added multi threading to the socket communication

--- OmronToRasp/Server.py
+++ OmronToRasp/Server.py
@@ -1,6 +1,7 @@
 import socket
 import pickle
 import time
+import threading
 
 # This function creates a socket server for sending data
 
@@ -16,12 +17,23 @@ def socketServer(ipaddr, port, timeout):
     messagedict = pickle.loads(message)
     return(messagedict)
 
-'''
-while(1):
-    test_dict = socketServer('192.168.0.249', 124)
+def socketServerMultiThreading(ipaddr, port, timeout, queue):
 
-    for k, v in test_dict.items():
-        test_dict[k] = 10
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    print(test_dict)
-'''
+    try:
+        server_socket.bind((ipaddr, port))
+    except socket.error as e:
+        print(e)
+
+    finally:
+        server_socket.listen(1)
+    #    server_socket.settimeout(timeout) # for testing purposes
+        client_connected, client_address = server_socket.accept()
+        message = client_connected.recv(1024)
+        messagedict = pickle.loads(message)
+        time.sleep(10)
+        queue.put(messagedict)
+
+    
+ 
